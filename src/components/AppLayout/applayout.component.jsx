5d90f1ce226fb50cba8262adf47e24920cb2cd45
dorjepadma <dorjepadma@gmail.com>
2022-08-26 17:07:39 -0700

    added MDB side bar adjusted color

--- src/components/AppLayout/applayout.component.jsx
+++ src/components/AppLayout/applayout.component.jsx
@@ -3,7 +3,7 @@ import Navigation from "../navigation/navigation.component";
 
 const AppLayout = () => {
     return <div style={{
-        padding: '50px 0px 0px 370px'
+        padding: '0px 0px 0px 0px'
     }}>
         <Navigation />
         <Outlet />
