5ce080202fb0db7448854e34761e377c48ced112
dorjepadma <dorjepadma@gmail.com>
2022-09-09 12:00:15 -0700

    header updated, mobile responsiveness 90%

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -8,7 +8,9 @@ import './homepage.styles.scss';
 const Homepage = () => {
   return (
     <div className="homepage">
-   
+    <div container className='pageHeader' col='2'>
+        <h1> Andrea Traber</h1>
+      </div>
         <div className='subTitle'> Visions of the 5 elements
         </div>
 
