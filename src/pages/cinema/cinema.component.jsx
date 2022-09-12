13d504f3f741b097847b70c243c0b4e4762a8a8c
dorjepadma <dorjepadma@gmail.com>
2022-09-12 10:43:28 -0700

    movie page updated with background and hero component

--- src/pages/cinema/cinema.component.jsx
+++ src/pages/cinema/cinema.component.jsx
@@ -3,7 +3,10 @@ import './cinema.styles.scss';
 const Cinema = () => {
   return (
     <div className="cinema">
-      <h1> Have you come to watch a movie? </h1>
+      <div className='cinemaHero'>
+
+      <h1 className='theaterTitle'> Have you come to watch a movie? </h1>
+      </div>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/VA04DTP5rKA" title="YouTube video player" frameborder="3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
