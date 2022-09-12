9c882da25c0d32b77cdd08029f92750b8eab6c5b
dorjepadma <dorjepadma@gmail.com>
2022-09-12 11:21:31 -0700

    andrea page background created

--- src/pages/andrea/Andrea.component.jsx
+++ src/pages/andrea/Andrea.component.jsx
@@ -7,14 +7,16 @@ import './andrea.styles.scss';
 
 const Andrea = () => {
   return (
-    <div className="homepage">
-      <div className='titleContainer'>
-        <div className='title'>
+    <div className="andreaContainer">
+     
+        <div className='andreaHero'>
+        <div className='andreaTitle'>
           <h1>Andrea Traber</h1>
+        </div>
         </div>
         <div className='subTitle'> Unfolding Wisdom through paint, brush, and dance.
         </div>
-      </div>
+    
       <img className='homePageImage' src={AndreaImage} alt='Andrea'>
 
       </img>
--- src/pages/cinema/cinema.component.jsx
+++ src/pages/cinema/cinema.component.jsx
@@ -7,7 +7,9 @@ const Cinema = () => {
 
       <h1 className='theaterTitle'> Have you come to watch a movie? </h1>
       </div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/VA04DTP5rKA" title="YouTube video player" frameborder="3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div >
+      <iframe className='movieBox' src="https://www.youtube.com/embed/VA04DTP5rKA" title="YouTube video player" frameborder="3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     </div>
   );
 
