d83a8a39bb134e06e21db9a3d75a31c38c61e40e
dorjepadma <dorjepadma@gmail.com>
2022-09-22 14:23:22 -0700

    added instagram page styling

--- src/pages/social/social.component.jsx
+++ src/pages/social/social.component.jsx
@@ -1,10 +1,22 @@
-import Curator from './Curator.component';
+// import Curator from './Curator.component';
+import { MDBRipple } from 'mdb-react-ui-kit';
 import './social.styles.scss';
+import Instagram from '../../assets/images/Social/instagram.png';
 
 const Social = () => {
   return (
     <div className="socialContainer">
       <h1> A curated space for social media. Tag Andrea and her works and have your post show up here on our Social Page. We look forward to seeing your creativity. </h1>
+      <MDBRipple rippleTag='a'>
+        <a href= 'https://www.instagram.com/andreatraber.studio/'>
+      <img
+        src={Instagram}
+        className='img-fluid rounded'
+        alt='Instagram'
+        href= 'https://www.instagram.com/andreatraber.studio/'
+      />
+      </a>
+    </MDBRipple>
       <div className='curatorContainer'>
       {/* <Curator feedId="d8aa9f35-b8eb-4e3c-9ee4-9a0dbb61cd92" /> */}
   </div>
