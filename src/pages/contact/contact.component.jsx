079d8abdd5fc7e0daab69b5424dac22a91bfc4ff
dorjepadma <dorjepadma@gmail.com>
2022-09-27 09:58:41 -0700

    contact form scss

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -6,24 +6,25 @@ const Contact = () => {
   return (
     <>
     <div className="contactContainer">
-      <h1> Reach Andrea here </h1>
-    <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
-    <h2>Contact us</h2>
+      <div className="contactForm">
+        <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
+          <h2>Reach Andrea here:</h2>
 
-    <MDBInput label='Name' v-model='name' wrapperClass='mb-4' />
+          <MDBInput label='Name' v-model='name' wrapperClass='mb-4' />
 
-    <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' />
+          <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' />
 
-    <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' />
+          <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' />
 
-    <MDBTextArea wrapperClass='mb-4' label='Message' />
+          <MDBTextArea wrapperClass='mb-4' label='Message' />
 
-    <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
+          <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
 
-    <MDBBtn color='primary' block className='my-4'>
-      Send
-    </MDBBtn>
-  </form>
+          <MDBBtn color='primary' block className='my-4'>
+            Send
+          </MDBBtn>
+        </form>
+      </div>
     </div>
   </>
   );
