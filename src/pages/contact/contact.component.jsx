947000228ac26481ffc18dc82a66ece97143a74d
dorjepadma <dorjepadma@gmail.com>
2022-09-27 13:41:34 -0700

    added validation fields to contact form

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from 'mdb-react-ui-kit';
+import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea, MDBValidationItem } from 'mdb-react-ui-kit';
 import './contact.styles.scss';
 
 const Contact = () => {
@@ -10,15 +10,25 @@ const Contact = () => {
         <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
           <h2>Reach Andrea here:</h2>
 
-          <MDBInput label='Name' v-model='name' wrapperClass='mb-4' />
+          <MDBValidationItem className='mb-3' required>
+          <MDBInput label='Name' v-model='name' wrapperClass='mb-4' required/>
+          </MDBValidationItem>
 
-          <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' />
+          <MDBValidationItem className='mb-3' required> 
+          <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' required/>
+          </MDBValidationItem>
 
-          <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' />
+          <MDBValidationItem className='mb-3' required>
+          <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' required/>
+          </MDBValidationItem>
 
-          <MDBTextArea wrapperClass='mb-4' label='Message' />
-
-          <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
+          <MDBValidationItem className='mb-3' required>
+          <MDBTextArea wrapperClass='mb-4' label='Message' required/>
+          </MDBValidationItem>
+          
+          <MDBValidationItem className='mb-3' required>
+          <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' required/>
+          </MDBValidationItem>
 
           <MDBBtn color='primary' block className='my-4'>
             Send
