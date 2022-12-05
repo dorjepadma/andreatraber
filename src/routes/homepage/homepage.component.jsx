47e52e41ed7e5c17657d89050618088067795f04
dorjepadma <dorjepadma@gmail.com>
2022-12-05 08:38:18 -0800

    adjusted homepage so it has a revolving carousel

--- src/routes/homepage/homepage.component.jsx
+++ src/routes/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 
 import React from 'react';
-
+import HomepageCarousel from './homepageCarousel.component';
 import {
   MDBCarousel,
   MDBCarouselItem,
+  MDBCard,
+  MDBContainer
 } from 'mdb-react-ui-kit';
 
 import Hung from '../../assets/images/hung.jpeg';
@@ -19,29 +21,8 @@ const Homepage = () => {
       <div  className='Title'>
           <h1> Visions of the Five Elements</h1>
       </div>
-      <div className='frontPieceContainer'>
-      <MDBCarousel fade className='collectionsCarousel' interval={1000}>
-      <div className="col-lg-12">
-      <MDBCarouselItem
-        className='w-100 d-block active'
-        itemID={0}
-      >
-      <img src={Hung} className='img-fluid shadow-4'   alt='the painting hung from the View collection' />
-      </MDBCarouselItem>
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemID={1}
-      >
-      <img src={ThatsHow} className='img-fluid shadow-4' alt='Thats how the light gets in from the Expressions collection' />
-      </MDBCarouselItem>
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemID={2}
-      >
-      <img src={Ribbit} className='img-fluid shadow-4' alt='Ribbit from the Nature is Medicine collection' />
-      </MDBCarouselItem>
-      </div>
-    </MDBCarousel>
+      <div className='frontPiece'>
+      <HomepageCarousel />
       </div>
     </div>
   );
